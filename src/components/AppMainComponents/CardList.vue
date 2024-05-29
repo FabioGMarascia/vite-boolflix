3367f97c169bd02d5f6fcb89b08f921d56032113
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-29 22:53:29 +0200

    M-3 1/2

--- src/components/AppMainComponents/CardList.vue
+++ src/components/AppMainComponents/CardList.vue
@@ -20,17 +20,16 @@ export default {
 
 <template>
 	<span class="title text-black rounded py-3 px-4 fw-bold fs-5 border border-2">FILMS</span>
-	<div class="filmBox row mx-0 justify-content-center mt-5">
+	<div class="filmBox row mx-0 justify-content-center mt-4">
 		<FilmCard v-for="card in store.filmList" :element="card" />
 	</div>
 
 	<hr class="text-white border-5 my-5" />
 
 	<span class="title text-black rounded py-3 px-4 fw-bold fs-5 border border-2">TV SERIES</span>
-	<div class="tvSeriesBox row mx-0 justify-content-center mt-5">
+	<div class="tvSeriesBox row mx-0 justify-content-center mt-4">
 		<TvSeriesCard v-for="card in store.tvSeriesList" :element="card" />
 	</div>
-	Box
 </template>
 
 <style scoped>
--- src/components/AppMainComponents/TvSeriesCard.vue
+++ src/components/AppMainComponents/TvSeriesCard.vue
@@ -7,9 +7,9 @@ export default {
 
 <template>
 	<div class="col-2 mb-3 px-0 m-4">
-		<!-- <img :src=""> -->
+		<img :src="`https://image.tmdb.org/t/p/w342` + element.poster_path" class="w-100" />
 
-		<div class="cardInfoBox text-center py-4 border border-2 rounded bg-white">
+		<div class="cardInfoBox text-center py-4 px-2 border border-2 rounded bg-white">
 			<h5 class="fw-bold">{{ "Title: " + element.name }}</h5>
 			<div>{{ "Original title: " + element.original_name }}</div>
 			<div>{{ "Lenguage: " + element.original_language }}</div>
@@ -18,4 +18,12 @@ export default {
 	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.cardInfoBox {
+	height: 12rem;
+}
+
+img {
+	height: 27rem;
+}
+</style>
