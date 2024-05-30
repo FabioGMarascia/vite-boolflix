a4ca21ca3605bd8e43177804e9007d7ba3bc0bd5
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-30 10:17:48 +0200

    img placeholder

--- src/components/AppMainComponents/FilmCard.vue
+++ src/components/AppMainComponents/FilmCard.vue
@@ -2,12 +2,30 @@
 export default {
 	name: "FilmCard",
 	props: { element: Object },
+	data() {
+		return {
+			img: null,
+			placeholderImgPath: `placeholder-locandine.jpg`,
+		};
+	},
+	methods: {
+		getImg(path) {
+			let risultato = new URL(`../../assets/` + path, import.meta.url);
+			return risultato.href;
+		},
+	},
 };
 </script>
 
 <template>
 	<div class="col-2 mb-3 px-0 m-4">
-		<img :src="`https://image.tmdb.org/t/p/w342` + element.poster_path" class="w-100" />
+		<img
+			:src="
+				element.poster_path != null
+					? `https://image.tmdb.org/t/p/w342` + element.poster_path
+					: getImg(placeholderImgPath)
+			"
+			class="w-100" />
 
 		<div class="cardInfoBox text-center py-4 px-2 border border-2 rounded bg-white">
 			<h5 class="fw-bold">{{ "Title: " + element.title }}</h5>
